34b5a9a76387edd014e9b34ae751f86819f45214
Shahnoza Nosirkhodjaeva <Shahnoza_Nosirkhodjaeva@student.itpu.uz>
2025-08-19 11:32:43 +0500

    footer fixed

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,15 +1,17 @@
 export default function Footer() {
-    return (
-      <footer className="container text--gray-900 py-6 text-center m-auto">
-        <div className="my-5"> <a
-                href="/policy"
-                className="underline text-gray-950"
-              >
-                Политика конфиденциальности
-              </a>
-            </div>
-        <p className="text-sm">© {new Date().getFullYear()} Portfolio Akhmadzhanov. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="container text--gray-900 py-6 text-center m-auto">
+      <div className="my-5">
+        <a
+          href="/policy"
+          className="underline text-gray-950 text-xs md:text-sm"
+        >
+          Политика конфиденциальности
+        </a>
+      </div>
+      <p className="text-xs md:text-sm">
+        © {new Date().getFullYear()} Portfolio Akhmadzhanov. All rights reserved.
+      </p>
+    </footer>
+  );
+}
